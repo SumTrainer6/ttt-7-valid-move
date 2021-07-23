a1df1a22c045783485086d3bd14ef12661cc5a27
SumTrainer6 <summertrainer@yahoo.com>
2021-07-23 11:14:05 -0400

    commit tree 4f21562d22c199ad54f6d6a39e96f3e855f5c48b

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,3 +2,10 @@
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def valid_move?(board, position)
+    if (board[position] ==  " " || board[position] == "")
+        true
+    else
+        false
+    end
+end 
